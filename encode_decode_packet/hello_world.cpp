b8509ec4f45c21805457e4f4889db6adc09fb74a
alpha-gj <Guang-Jie_Jhang@alphanetworks.com>
2018-12-06 17:35:21 +0800

    Finish encode_decode_packet

--- encode_decode_packet/hello_world.cpp
+++ encode_decode_packet/hello_world.cpp
@@ -17,8 +17,16 @@ using namespace std;
 /* MCU_PACKET_SIZE*/
 #define MCU_PACKET_SIZE				120
 
-#define HEAD_BYTE_NUMS				1
-/* Need to know how long is header of MCU_PACKET */
+/* For mcu packet*/
+#define MCU_PACKET_HEADER_BYTES				1
+// 1 byte: how long is mcu packet
+
+/* For sub mcu packet*/
+#define SUB_MCU_PACKET_HEADER_BYTES			2
+#define OFFSET_SUB_MCU_HEADER_DATA_TYPE		0
+#define OFFSET_SUB_MCU_HEADER_PAYLOAD_LEN	1
+// 1 byte: data type of sub mcu packet
+// 1 byte: payload len of sub mcu packet
 
 typedef struct {
 	char data_type;
@@ -26,65 +34,54 @@ typedef struct {
 	char payload_context[PAYLOAD_SIZE];
 } MCU_SUB_PACKET;
 
-int decodeMcuPacket(char *mcu_packet) 
+/*  DECODE */
+int decodeMcuPacket(char mcu_packet[]) 
 {
-	char out_buffer[128] = {0};
-	int read_count = mcu_packet[0] - HEAD_BYTE_NUMS;
-	int sub_mcu_packet_start_count = 1;
-	
-	/* For sub_mcu_packet */
-	unsigned char sub_mcu_packet_payload_len;
-	
-	while (read_count > 0) {
+	char out_buffer[MCU_PACKET_SIZE] = {0};
+	int total_sub_mcu_packets_len = mcu_packet[0] - MCU_PACKET_HEADER_BYTES;
+	int sub_mcu_packet_start_count = MCU_PACKET_HEADER_BYTES; //first sub mcu packet start
+	unsigned char sub_mcu_packet_payload_len = 0;
+	int	read_count = 0;
+
+	while (read_count < total_sub_mcu_packets_len) {
 
 		memset(&out_buffer, 0, sizeof(out_buffer));
 
 		switch (mcu_packet[sub_mcu_packet_start_count]) {
+
 			case MCU_DATA_FW_VER:
 				printf("case: MCU_DATA_FW_VER\n");
-				printf("type is %x\n", mcu_packet[sub_mcu_packet_start_count]);
-				sub_mcu_packet_payload_len = mcu_packet[sub_mcu_packet_start_count + 1];
-				printf("len is %d\n", sub_mcu_packet_payload_len);
-				memcpy(out_buffer, (const char *)&mcu_packet[sub_mcu_packet_start_count+2], sub_mcu_packet_payload_len);
-				out_buffer[sub_mcu_packet_payload_len] = '\0';
-				printf("MCU_DATA_FW_VER is %s\n", out_buffer);
-				printf("read_count is %d\n", read_count);
 				break;
+
 			case MCU_DATA_CAM_NAME:
 				printf("case: MCU_DATA_CAM_NAME\n");
-				printf("type is %x\n", mcu_packet[sub_mcu_packet_start_count]);
-				sub_mcu_packet_payload_len = mcu_packet[sub_mcu_packet_start_count + 1];
-				printf("len is %d\n", sub_mcu_packet_payload_len);
-				memcpy(out_buffer, (const char *)&mcu_packet[sub_mcu_packet_start_count+2], sub_mcu_packet_payload_len);
-				out_buffer[sub_mcu_packet_payload_len] = '\0';
-				printf("MCU_DATA_CAM_NAME is %s\n", out_buffer);
-				printf("read_count is %d\n", read_count);
 				break;
+
 			case MCU_DATA_WIFI_STATUS:
 				printf("case: MCU_DATA_WIFI_STATUS\n");
-				printf("type is %x\n", mcu_packet[sub_mcu_packet_start_count]);
-				sub_mcu_packet_payload_len = mcu_packet[sub_mcu_packet_start_count + 1];
-				printf("len is %d\n", sub_mcu_packet_payload_len);
-				memcpy(out_buffer, (const char *)&mcu_packet[sub_mcu_packet_start_count+2], sub_mcu_packet_payload_len);
-				out_buffer[sub_mcu_packet_payload_len] = '\0';
-				printf("MCU_DATA_WIFI_STATUS is %s\n", out_buffer);
-				printf("read_count is %d\n", read_count);
-				break;
-			default:
-				printf("case: No match\n");
 				break;
 
+			default:
+				printf("case: No match, exit\n");
+				exit(0);
 		}
 
-		read_count = read_count - 2 - sub_mcu_packet_payload_len;
-		printf("main read_count is %d\n", read_count);
-		sub_mcu_packet_start_count = sub_mcu_packet_start_count + 2 + sub_mcu_packet_payload_len;
+		/* Printf sub muc packet */
+		printf("data type: %x\n", mcu_packet[sub_mcu_packet_start_count]);
+
+		sub_mcu_packet_payload_len = mcu_packet[sub_mcu_packet_start_count + OFFSET_SUB_MCU_HEADER_PAYLOAD_LEN];
+		printf("patload_len is %d\n", sub_mcu_packet_payload_len);
+
+		memcpy(out_buffer, (const char *)&mcu_packet[sub_mcu_packet_start_count + SUB_MCU_PACKET_HEADER_BYTES], sub_mcu_packet_payload_len);
+		out_buffer[sub_mcu_packet_payload_len] = '\0';
+		printf("patload_context is %s\n\n", out_buffer);
+
+		/* decide Next sub mcu packet location */
+		read_count = read_count + SUB_MCU_PACKET_HEADER_BYTES + sub_mcu_packet_payload_len;
+		sub_mcu_packet_start_count = sub_mcu_packet_start_count + SUB_MCU_PACKET_HEADER_BYTES + sub_mcu_packet_payload_len;
+
 	}
 
-	printf("read_count is %d\n", read_count);
-	
-
-//	printf("This is %s\n", mcu_packet);
 	return 0;
 }
 
@@ -92,7 +89,7 @@ int decodeMcuPacket(char *mcu_packet)
 int encodeToMcuPacket(vector<MCU_SUB_PACKET> vectorMcuPacket, char mcu_packet[])
 {
 	char *mcu_packet_header_buffer_iter = mcu_packet;
-	char *sub_mcu_packet_buffer_iter = mcu_packet + HEAD_BYTE_NUMS;
+	char *sub_mcu_packet_buffer_iter = mcu_packet + MCU_PACKET_HEADER_BYTES;
 
 	for (int i = 0; i < vectorMcuPacket.size(); i++) {
 		
@@ -120,14 +117,14 @@ int encodeToMcuPacket(vector<MCU_SUB_PACKET> vectorMcuPacket, char mcu_packet[])
 	}
 
 	/* Cal the total len of sub mcu packets */
-	int total_sub_mcu_packet_len = sub_mcu_packet_buffer_iter - &mcu_packet[HEAD_BYTE_NUMS];
+	int total_sub_mcu_packet_len = sub_mcu_packet_buffer_iter - &mcu_packet[MCU_PACKET_HEADER_BYTES];
 
 	/* Set the mcu_packet len to header location of mcu packet */
-	*mcu_packet_header_buffer_iter = HEAD_BYTE_NUMS + total_sub_mcu_packet_len;
+	*mcu_packet_header_buffer_iter = MCU_PACKET_HEADER_BYTES + total_sub_mcu_packet_len;
 	
 	fprintf(stderr, "MCU packet len is %d, include %d byte of header and %d byte of payload (all sub mcu packet len)\n",
 			mcu_packet[0], 
-			HEAD_BYTE_NUMS, 
+			MCU_PACKET_HEADER_BYTES, 
 			total_sub_mcu_packet_len);
 
 	/* Check the mcu packet size */
@@ -166,10 +163,12 @@ int main(void)
 	vectorMcuPacket.push_back(init_mcu_sub_packet(MCU_DATA_WIFI_STATUS, wifi_status));
 
 	/* STEP2. Encode multiple mcu sub packets into a mcu packet */
+	fprintf(stderr, "\n ****** Encode ******\n");
 	char mcu_packet[MCU_PACKET_SIZE] = {0};
 	encodeToMcuPacket(vectorMcuPacket,  mcu_packet);
 
 	/* STEP3. Decode the mcu packet into multiple mcu sub packets*/
+	fprintf(stderr, "\n ****** Decode ******\n");
 	decodeMcuPacket(mcu_packet);
 
 	return 0;
